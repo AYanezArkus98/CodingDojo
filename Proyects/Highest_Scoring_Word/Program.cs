3aa06e88a3b4936386c27159fd3c4d0234c57c5d
AYanezArkus98 <ayanez@arkusnexus.com>
2024-09-19 15:40:58 -0700

    Highest_Scoring_Word

--- Proyects/Highest_Scoring_Word/Program.cs
+++ Proyects/Highest_Scoring_Word/Program.cs
@@ -8,8 +8,47 @@ namespace Highest_Scoring_Word
 {
     internal class Program
     {
+
+        public static int HighestScore(string word)
+        {
+            char[] abecedario = new char[] {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            };
+            int totalScore = 0;
+
+            foreach (char letra in word)
+            {
+                char letraMinuscula = char.ToLower(letra);
+
+                int posicion = Array.IndexOf(abecedario, letraMinuscula) + 1;
+
+                totalScore += posicion;
+            }
+
+            return totalScore;
+        }
         static void Main(string[] args)
         {
+            string word1 = "Alonso";
+            string word2 = "Aaron";
+
+            int scoreWord1 = HighestScore(word1);
+            int scoreWord2 = HighestScore(word2);
+
+            if (scoreWord1 > scoreWord2)
+            {
+                Console.WriteLine(word1);
+            }
+            else if (scoreWord2 > scoreWord1)
+            {
+                Console.WriteLine(word2);
+            }
+            else
+            {
+                Console.WriteLine(word1); 
+            }
+            Console.ReadKey();
         }
     }
 }
