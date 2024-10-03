e510f494c7051cec0d0a0eb7492f32117cff1b90
AYanezArkus98 <ayanez@arkusnexus.com>
2024-10-03 13:48:57 -0700

    update

---
+++ Proyects/Hashtag/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Here # are some hash#tags: #hello, ##double, #123, and# #validHash";
+        string pattern = @"(?<!\S)#+([a-zA-Z]+)\b";
+
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        List<string> hashtags = new List<string>();
+
+        foreach (Match match in matches)
+        {
+            hashtags.Add(match.Groups[1].Value);
+        }
+
+        Console.WriteLine("Hashtags válidos:");
+        foreach (string tag in hashtags)
+        {
+            Console.WriteLine(tag);
+        }
+        Console.ReadKey();
+    }
+}
