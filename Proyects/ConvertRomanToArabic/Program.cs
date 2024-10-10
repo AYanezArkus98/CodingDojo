6fed1b8ee3ecbae3531aff6b18f0b3b897934200
AYanezArkus98 <ayanez@arkusnexus.com>
2024-10-10 15:28:34 -0700

    RomanToArabic

---
+++ Proyects/ConvertRomanToArabic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RomanToArabic("XIV")); // Salida: 14
+        Console.WriteLine(RomanToArabic("MCMXCIV")); // Salida: 1994
+        Console.ReadKey();
+    }
+
+    public static int RomanToArabic(string roman)
+    {
+        Dictionary<char, int> romanMap = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int arabic = 0;
+        for (int i = 0; i < roman.Length; i++)
+        {
+            if (i + 1 < roman.Length && romanMap[roman[i]] < romanMap[roman[i + 1]])
+            {
+                arabic -= romanMap[roman[i]];
+            }
+            else
+            {
+                arabic += romanMap[roman[i]];
+            }
+        }
+
+        return arabic;
+    }
+}
